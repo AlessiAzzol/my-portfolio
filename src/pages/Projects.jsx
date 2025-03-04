1162b2d43aee11c170e2a0a43fcb8ab1c33f532c
Alessia <azzolina.alessia@gmail.com>
2025-03-04 18:08:51 +0100

    project carousel

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -11,14 +11,21 @@ import GitHubIcon from '@mui/icons-material/GitHub';
 import styled from '@mui/material/styles/styled';
 import Collapse from '@mui/material/Collapse';
 import CardActions from '@mui/material/CardActions';
+import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
+import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Navigation, } from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import shadows from '@mui/material/styles/shadows.js';
+
 
 
 const ExpandMore = styled((props) => {
 	const { expand, ...other } = props;
 	return <IconButton {...other} />;
-})(({ theme }) => ({
+})(() => ({
 	marginLeft: 'auto',
-
 	variants: [
 		{
 			props: ({ expand }) => !expand,
@@ -39,8 +46,11 @@ function Portfolio() {
 	const { data } = useLanguage();
 	const [expanded, setExpanded] = React.useState(false);
 
-	const handleExpandClick = () => {
-		setExpanded(!expanded);
+	const handleExpandClick = (index) => {
+		setExpanded((prev) => ({
+			...prev,
+			[index]: !prev[index],
+		}));
 	};
 	return (
 
@@ -52,54 +62,73 @@ function Portfolio() {
 		}}>
 			<Typography variant="h6">{data.pages.portfolio.title}</Typography>
 			<Typography >{data.pages.portfolio.description}</Typography>
-			{data.pages.portfolio.projects.map((project, index) => (
-				<Card key={`projectBox` + index} sx={{
-					width: 345,
-					margin: 2,
-				}}>
-					<CardHeader key={`header` + index}
-						title={project.title}
-						subheader={project.subheader}
-					/>
-					<CardMedia key={`media` + index}
-						component="img"
-						height="194"
-						image={project.img}
-					/>
-					<CardContent key={`shortBody` + index}>
-						<Typography variant="body2" sx={{ color: 'text.secondary' }}>
-							{project.shortDescription}
-						</Typography>
-					</CardContent>
-					<CardActions disableSpacing>
-						<IconButton key={`gitButton` + index} aria-label="github" href={project.reference}
-							target="_blank">
-							<GitHubIcon />
-						</IconButton>
-						<ExpandMore
-							expand={expanded}
-							onClick={handleExpandClick}
-							aria-expanded={expanded}
-							aria-label="show more"
-						>
-							<ExpandMoreIcon />
-						</ExpandMore>
-					</CardActions>
+
+			<ArrowBackIosIcon fontSize='large' className="swiper-button-prev" />
+
+			<Swiper
+				modules={[Navigation]}
+				navigation={{
+					prevEl: '.swiper-button-prev',
+					nextEl: '.swiper-button-next',
+				}}
+				slidesPerView={2}
+				loop={true}
+				grabCursor={true}
+			>
+
+				{data.pages.portfolio.projects.map((project, index) => (
+					<SwiperSlide key={index}>
+						<Card key={`projectBox` + index} sx={{
+
+							margin: 3,
+							backgroundColor: 'rgba(255, 255, 255, 0.6)',
+							backgroundImage: 'none',
+						}}>
+							<CardHeader key={`header` + index}
+								title={project.title}
+								subheader={project.subheader}
+							/>
+							<CardMedia key={`media` + index}
+								component="img"
+								height="194"
+								image={project.img}
+							/>
+							<CardContent key={`shortBody` + index}>
+								<Typography variant="body2" sx={{ color: 'text.secondary' }}>
+									{project.shortDescription}
+								</Typography>
+							</CardContent>
+							<CardActions disableSpacing>
+								<IconButton key={`gitButton` + index} aria-label="github" href={project.reference}
+									target="_blank">
+									<GitHubIcon />
+								</IconButton>
+								<ExpandMore
+									expand={expanded}
+									onClick={() => handleExpandClick(index)}
+									aria-expanded={expanded[index] || false}
+									aria-label="show more"
+								>
+									<ExpandMoreIcon />
+								</ExpandMore>
+							</CardActions>
+							<Collapse in={expanded[index] || false} timeout="auto" unmountOnExit>
+								<CardContent key={`longBody` + index}>
+									<Typography variant="body2" sx={{ color: 'text.secondary' }}>
+										{project.longDescription}
+									</Typography>
+								</CardContent>
+							</Collapse>
+						</Card>
+					</SwiperSlide>
+				))
+				}
+			</Swiper >
 
 
-					<Collapse in={expanded} timeout="auto" unmountOnExit>
-						<CardContent key={`longBody` + index}>
-							<Typography variant="body2" sx={{ color: 'text.secondary' }}>
-								{project.longDescription}
-							</Typography>
-						</CardContent>
-					</Collapse>
-				</Card>
-			))
-			}
+			<ArrowForwardIosIcon fontSize='large' className="swiper-button-next" />
 
 		</Card>
-
 
 
 	);
